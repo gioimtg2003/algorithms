0ef10142d5170691da1b223ed10a710341326ae9
gioimtg2003 <conggioi.pro264@gmail.com>
2024-04-22 17:45:37 +0700

    problem 10+11

--- docs/algorithms/largestPalindromeProduct.ts
+++ docs/algorithms/largestPalindromeProduct.ts
@@ -2,6 +2,7 @@
  * A palindromic number reads the same both ways. The largest palindrome made from the product of two 2-digit numbers is 9009 = 91 × 99.
  * Find the largest palindrome made from the product of two n-digit numbers.
  * @param n
+ * @example
  * largestPalindromeProduct(3) should return 906609.
  * largestPalindromeProduct(2) should return 9009.
  */
---
+++ docs/algorithms/primeSummation.ts
@@ -0,0 +1,34 @@
+/**
+ * The sum of the primes below 10 is 2 + 3 + 5 + 7 = 17.
+ *
+ * Find the sum of all the primes below n.
+ * @param n
+ * @returns
+ * @example
+ * primeSummation(17) should return 41.
+ * primeSummation(2001) should return 277050.
+ * primeSummation(140759) should return 873608362.
+ */
+function primeSummation(n: number) {
+    let i = 2;
+    let s = 0;
+    while (i < n) {
+        if (isPrime(i)) {
+            s += i;
+        }
+        i++;
+    }
+    return s;
+}
+
+function isPrime(n: number) {
+    for (let i = 2; i <= Math.sqrt(n); i++) {
+        if (n % i === 0) {
+            return false;
+        }
+    }
+    return n > 1;
+}
+
+console.log(primeSummation(2000000));
+console.log(isPrime(4));
